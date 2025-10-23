7d518e5a2e6fb51e08f49f49512f2224f171bf62
Назар Берещук <nazarberehklas@gmail.com>
2025-10-23 18:11:50 +0300

    Cart html and Seller profile

---
+++ Controllers/SellerProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prosto.Controllers
+{
+    public class SellerProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
